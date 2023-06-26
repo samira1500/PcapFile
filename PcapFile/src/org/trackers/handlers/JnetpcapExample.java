e7a4d1466851e99edb6d32f8efb87211d83a90ff
samira1500 <samira.tofik1500@gmail.com>
2023-06-26 15:11:16 +0300

    sdsd

--- PcapFile/src/org/trackers/handlers/JnetpcapExample.java
+++ PcapFile/src/org/trackers/handlers/JnetpcapExample.java
@@ -1,14 +1,5 @@
 package org.trackers.handlers;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.jnetpcap.Pcap;
-import org.jnetpcap.PcapIf;
-import org.jnetpcap.packet.PcapPacket;
-import org.jnetpcap.packet.PcapPacketHandler;
-import org.jnetpcap.protocol.network.Ip4;
-import org.jnetpcap.protocol.network.Icmp;
-
 public class JnetpcapExample {
 
   public static void main(String[] args) throws Exception { }
